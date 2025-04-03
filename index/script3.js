5f500524c0101175fbad93ddbc8bfcdead0dc7f2
CamilaSimone <simone.camila@escolar.ifrn.edu.br>
2025-04-03 17:19:14 -0300

    "dia 5 eu acho"

--- index/script3.js
+++ index/script3.js
@@ -1,7 +1,64 @@
 document.getElementById("texto1").onclick = function() {
-    window.location.href = "index2.html"; // Substitua pelo link desejado
+    window.location.href = "index2.html";
 };
 document.getElementById("texto2").onclick = function() {
-    window.location.href = "index4.html"; // Substitua pelo link desejado
+    window.location.href = "index4.html";
 };
+
+function abrirPopup() {
+    let msg = document.getElementById("popupOverlay");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+function abrirPopup2() {
+    let msg = document.getElementById("popupOverlay2");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+function abrirPopup3() {
+    let msg = document.getElementById("popupOverlay3");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+function abrirPopup4() {
+    let msg = document.getElementById("popupOverlay4");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+function abrirPopup5() {
+    let msg = document.getElementById("popupOverlay5");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+function abrirPopup6() {
+    let msg = document.getElementById("popupOverlay6");
+    msg.style.display = "flex"; 
+        
+        setTimeout(function() {
+            msg.style.display = "none";
+        }, 1500); // 1500 milissegundos = 1 segundo e meio
+};
+
+
+
 
